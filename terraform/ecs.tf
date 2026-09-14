e1b03b090d593d9d11740b23e1c570a35dc88bf4
dilip98914 <dilip98914@gmai.com>
2026-09-14 13:20:13 +0530

    initial commit

---
+++ terraform/ecs.tf
@@ -0,0 +1,10 @@
+resource "aws_ecs_cluster" "app" {
+  name = "${var.project_name}-cluster"
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+  tags = {
+    Project = "${var.project_name}"
+  }
+}
---
+++ terraform/sg.tf
@@ -0,0 +1,49 @@
+resource "aws_security_group" "alb" {
+  name        = "${var.project_name}-alb"
+  description = "Security group for public ALB"
+  vpc_id      = data.aws_vpc.default.id
+  ingress = {
+    description = "http from internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress = {
+    description = "allow outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name    = "${var.project_name}-alb"
+    Project = var.project_name
+  }
+}
+
+resource "aws_security_group" "ecs" {
+  name        = "${var.project_name}-ecs"
+  description = "Security group for ECS task"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress = {
+    description     = "http from alb only"
+    from_port       = 3000
+    to_port         = 3000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+  egress = {
+    description = "allow outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name    = "${var.project_name}-ecs"
+    Project = var.project_name
+  }
+}
+
