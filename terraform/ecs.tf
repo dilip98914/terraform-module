121cc266e779feacaad83d69c52476043ce6fd57
dilip98914 <dilip98914@gmail.com>
2026-09-14 16:43:12 +0530

    set desired count to 2

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -72,7 +72,7 @@ resource "aws_ecs_service" "app" {
   cluster = aws_ecs_cluster.app.id
   task_definition = aws_ecs_task_definition.app.arn
   
-  desired_count = 1
+  desired_count = 2
   launch_type = "FARGATE"
   network_configuration {
     subnets = data.aws_subnets.default.ids
