a5225a3b2f496bc2ea26437c8610494dd052c2a2
dilip98914 <dilip98914@gmai.com>
2026-09-14 16:25:36 +0530

    added ecs service

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -66,3 +66,33 @@ resource "aws_ecs_task_definition" "app" {
     }
   ])
 }
+
+resource "aws_ecs_service" "app" {
+  name=var.project_name
+  cluster = aws_ecs_cluster.app.id
+  task_definition = aws_ecs_task_definition.app.arn
+  
+  desired_count = 1
+  launch_type = "FARGATE"
+  network_configuration {
+    subnets = data.aws_subnets.default.ids
+    security_groups = [
+      aws_security_group.ecs.id
+    ]
+    assign_public_ip = true
+  }
+
+# Register this service's containers with this target group.
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app.arn
+    container_name = var.project_name
+    container_port = 3000
+  }
+
+  tags = {
+    Project     = var.project_name
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+
+}
